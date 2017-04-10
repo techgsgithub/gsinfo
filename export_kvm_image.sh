56e4d8caf0dd1ccfcb195e4a795fc5fe82e3a6dc
techgsgithub <techgsgithub@users.noreply.github.com>
2017-04-10 09:33:18 +0000

    Update export_kvm_image.sh

--- export_kvm_image.sh
+++ export_kvm_image.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+!/bin/bash
 #Author GS
-#Program to create full VM
+#Program to create full export VM 
 clear
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     printf "Only root user can run this program!, Sorry"
@@ -11,6 +11,38 @@ if [ $# -ne 2 ]; then
     printf "Format of the command is : exportvm name_of_current_vm  name_of_exporting_vm \n"
     printf "Retry with above command with correct syntax ! \n"
     exit 1
+fi
+STATE=`virsh dominfo $1 | grep "State" | cut -d " " -f 11`
+
+if [ "$STATE" = "running" ]; then
+	
+	printf "\n"
+	printf "Your VM $1 is in running condition, hence shut-down of  $1 is necessory. \n"
+	printf "Otherwise, you will get corrupt Exported image. \n"
+	read -p "Do you want to stop the VM ? (Y/N)" prompt 
+	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+        then
+		TIMEOUT=300
+		END_TIME=$(date -d "$TIMEOUT seconds" +%s)
+		printf "Trying to shutdown the VM $1 for export \n" 
+		virsh shutdown $1  --mode acpi &> /dev/null
+	sleep 5
+	printf "Recheking once again, just a second. \n"
+	fi
+	STATE=`virsh dominfo $1 | grep "State" | cut -d " " -f 11`
+	if [ "$STATE" = "running" ]; then
+        	printf "Shutdown process is still on, wait a while !"
+		sleep 5
+	fi
+	STATE=`virsh dominfo $1 | grep "State" | cut -d " " -f 11`
+        if [ "$STATE" = "running" ]; then
+		printf "Have some problem .. Existing.  Try to force-shudown $1 from GUI. \n"
+		printf " Bye !!!"
+	exit 0 
+		printf "Cancelled due to some problem. /n"
+	fi
+
+printf "Shutdown of $1 is successful.  Taking further process for export! \n"
 fi
 printf "\n"
 printf "===================================================================================\n"
@@ -29,7 +61,7 @@ printf "Trying to Export VM:$1 The outfile will be $2_exported_vm.img \n"
 qemu-img convert -f qcow2 $1.img -O qcow2 $2_exported_vm.img  2> /dev/null & 
 #  echo dots while command is executing
 while ps |grep $! &>/dev/null; do
-        echo -n "..."
+        echo -n "... "
         sleep 2
 done
 printf "\n"
